fcb6e69b36cb02fe2c0a76631be94da37e3310b6
Ian Bird <ianukusa@gmail.com>
2025-06-30 19:46:43 -0500

    whitespace changes

--- normal.scm
+++ normal.scm
@@ -45,49 +45,57 @@
            ((quote)			; returns second element
 					; unevaluated
 	    (cadr exp))
-               ((cons) 			; creates a cons cell of its
+	   
+           ((cons) 			; creates a cons cell of its
 					; arguments, marking them for
 					; evaluation but delays
 					; evaluation until requested.
-		(cons (list 'closure (nth exp 1))
-                             (list 'closure (nth exp 2))))
-               ((car) 			; gets the first element of a
+	    (cons (list 'closure (nth exp 1))
+                  (list 'closure (nth exp 2))))
+	       
+           ((car) 			; gets the first element of a
 					; cons cell and forces it if
 					; needed.
-                (let ((r (car (normal-eval-impl (nth exp 1)))))
-                  (if (and (list? r)
-                           (eq? (car r) 'closure))
-                      (normal-eval-impl (cadr r))
-                      r)))
-               ((cdr) ; gets the second element of a cons cell and
-		      ; forces it if needed
-                (let ((r (cdr (normal-eval-impl (nth exp 1)))))
-                  (if (and (list? r)
-                           (not (null? r)) (eq? (car r) 'closure))
-                      (normal-eval-impl (cadr r))
-                      r)))
-               ((if) 			; evaluates the first
+            (let ((r (car (normal-eval-impl (nth exp 1)))))
+              (if (and (list? r)
+                       (eq? (car r) 'closure))
+                  (normal-eval-impl (cadr r))
+                  r)))
+	       
+           ((cdr)                       ; gets the second element of a
+					; cons cell and forces it if
+					; needed
+            (let ((r (cdr (normal-eval-impl (nth exp 1)))))
+              (if (and (list? r)
+                       (not (null? r)) (eq? (car r) 'closure))
+                  (normal-eval-impl (cadr r))
+                  r)))
+	       
+           ((if) 			; evaluates the first
 					; argument, if it's true then
 					; evaluates the 2nd arg and
 					; returns it, otherwise
 					; evaluates the 3rd arg and
 					; returns that.
-                (let ((pred (cadr exp))
-                      (consq (nth exp 2))
-                      (alt (nth exp 3)))
-                  (if (normal-eval-impl pred)
-                      (normal-eval-impl consq)
-                      (normal-eval-impl alt))))
-               ((lambda) 		; creates a new function
-		(list 'closure (nth exp 2) (nth exp 1)))
-               ((define) 		; binds a value to the symbol
+            (let ((pred (cadr exp))
+                  (consq (nth exp 2))
+                  (alt (nth exp 3)))
+              (if (normal-eval-impl pred)
+                  (normal-eval-impl consq)
+                  (normal-eval-impl alt))))
+	       
+           ((lambda)           		; creates a new function
+	    (list 'closure (nth exp 2) (nth exp 1)))
+	       
+           ((define)    		; binds a value to the symbol
 					; table
-		(set! *env* (cons (cons (nth exp 1) (nth exp 2))
-                                  *env*)))
-               (else 			; its a funcall so apply the
+	    (set! *env* (cons (cons (nth exp 1) (nth exp 2))
+                              *env*)))
+	       
+           (else 			; its a funcall so apply the
 					; rest of the arguments to the
 					; first argument
-                (apply-normal (normal-eval-impl (car exp)) (cdr exp)))))
+            (apply-normal (normal-eval-impl (car exp)) (cdr exp)))))
         
         ((else ; unrecognized type
           panic))))
@@ -97,13 +105,17 @@
   ;; implementation then call underlying apply after evaluating all
   ;; the args normally.  otherwise, substitute the unevaluated args in
   ;; to the function in place of the params and then evaluate it.
-      (cond ((procedure? proc) (apply proc (map normal-eval-impl args)))
-            ((and (list? proc) 
-                  (eq? (car proc) 'closure))
-                 (normal-eval-impl (substitute (nth proc 1) 
-                                               (map (partial apply cons)
-                                                    (zip (nth proc 2) args)))))
-            (else (normal-eval-impl (cons proc args)))))
+  (cond ((procedure? proc)
+	 (apply proc (map normal-eval-impl args)))
+	
+        ((and (list? proc) 
+              (eq? (car proc) 'closure))
+         (normal-eval-impl (substitute (nth proc 1) 
+                                       (map (partial apply cons)
+                                            (zip (nth proc 2) args)))))
+	
+        (else
+	 (normal-eval-impl (cons proc args)))))
 
 (define (force-all exp)
   ;; forces evaluation recursively until no further computation can be done.
@@ -134,13 +146,22 @@
 ;; special forms.
 (define (substitute form substitutions)
   (let ((replacement (assoc form substitutions)))
-    (cond (replacement (cdr replacement))
+    (cond (replacement
+	   (cdr replacement))
+	  
           ((list? form)
            (case (car form)
-                 ((quote) form)
-                 ((lambda) `(lambda ,(nth form 1) ,(substitute (nth form 2) substitutions)))
-                 ((define) `(define ,(nth form 1) ,(substitute (nth form 2) substitutions)))
-                 (else (map (lambda (x) (substitute x substitutions)) form))))
+             ((quote)
+	      form)
+	     
+             ((lambda)
+	      `(lambda ,(nth form 1) ,(substitute (nth form 2) substitutions)))
+	     
+             ((define)
+	      `(define ,(nth form 1) ,(substitute (nth form 2) substitutions)))
+	     
+             (else
+	      (map (lambda (x) (substitute x substitutions)) form))))
           (else form))))
 
                
