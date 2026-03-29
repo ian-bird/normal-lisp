a39bde9e90eb36f917f673f8418f7f7856b1836b
ian-bird <41979749+ian-bird@users.noreply.github.com>
2026-03-29 13:36:24 -0500

    fix bugs + add new optimization
    Fixed bug with incorrect environment capture for closures (capturing compile-time environment not creation-time), and added an optimization where the location of all variables, not just defined ones, is calculated at compile time, fully eliminating runtime dynamic lookup.

--- compile.scm
+++ compile.scm
@@ -1,47 +1,67 @@
-(define (last-cons seq)
-  (if (null? (cdr seq))
-      seq
-      (last-cons (cdr seq))))
+(define (some proc seq)
+  (cond ((null? seq) #f)
+	((proc (car seq)) (car seq))
+	(else (some proc (cdr seq)))))
+
+(define (any? proc seq)
+  (if (some proc seq)
+      #t
+      #f))
+
+(define (constantly c) (lambda (_) c))
+
+(define (partial fn . args) (lambda rest-args (apply fn (append args rest-args))))
+
+(define (last seq) (car (last-pair seq)))
 
 ;; creates a lookup function to get the variable mentioned.
 ;; the lookup uses no counters and should be as fast as a car/cdr chain.
 ;; if the variable is not found, it returns a function that performs
 ;; a dynamic lookup for the variable.
 (define (get-assoc env var)
-  (letrec ((1d (lambda (a-list)
-                 (if (eq? (caar a-list) var)
-                     (lambda (al) (car al))
-                     (let ((rest (1d (cdr a-list))))
-                           (lambda (al) (rest (cdr al))))))))
-    (if (any? (lambda (a-list) (assoc var a-list)) env)
-        (if (assoc var (car env))
-            (let ((rest (1d (car env))))
-              (lambda (env) (rest (car env))))
-            (let ((rest (get-assoc (cdr env) var)))
-              (lambda (env) (rest (cdr env)))))
-        (lambda (env)
-          (reduce (lambda (acc e)
-                    (if acc acc (assoc var e)))
-                  #f
-                  env)))))
+  (if (any? (partial assoc var) env)
+      (if (assoc var (car env))
+          (let ((rest (let loop ((a-list (car env)))
+			(if (eq? (caar a-list) var)
+			    (lambda (al) (car al))
+			    (let ((rest (loop (cdr a-list))))
+			      (lambda (al) (rest (cdr al))))))))	    
+            (lambda (env) (rest (car env))))
+          (let ((rest (get-assoc (cdr env) var)))
+            (lambda (env) (rest (cdr env)))))
+      (begin (add-global-binding! env var)
+	     (get-assoc env var))))
+
+;; add an unbound definition to the global environment
+;; since define can only run in the global level,
+;; anything not in the lexical scope at compile time must be present here.
+(define (add-global-binding! env var)
+  (if (null? (car (last-pair env)))
+      (set-car! (last-pair env) (list (list var)))
+      (set-cdr! (last-pair (car (last-pair env))) (list (list var)))))
+
+(define (map-args params args)
+  (cond ((and (null? params) (null? args)) '())
+	((symbol? params) (acons params args '()))
+	(else (acons (car params) (car args) (map-args (cdr params) (cdr args))))))
 
 ;; walks the code, combining lambdas into a fn that produces the desired behavior.
 ;; compiling thus splits syntactical analysis / symbol location from executing the code.
 ;; compilation yields a 5x speedup over naive interpretation.
 (define (compile-statement s example-env)
   (let ((check-arity (lambda (n)
-                       (unless (= (count s) (1+ n)) 
+                       (unless (= (length s) (1+ n)) 
                          (error (string-append
                                  "arity mismatch: expected " 
                                  (number->string n)
                                  " found "
-                                 (number->string (1- (count statement)))))))))
+                                 (number->string (1- (length s)))))))))
     (cond ((symbol? s)
            (let ((find-pair (get-assoc example-env s)))
              ;; if found then pass to k, otherwise eval and pass to k.
              (lambda (env k) 
                (let ((p (find-pair env)))
-                 (k (if p (cdr p) (eval s)))))))
+                 (k (if (and p (not (null? (cdr p)))) (cdr p) (eval s (current-module))))))))
           ((any? (lambda (proc) (proc s)) (list boolean? string? number?) )
            ;; self-evaluating. pass value to k directly.
            (lambda (_ k) (k s)))
@@ -59,56 +79,77 @@
                  ((cdr) (check-arity 1)
                   (let ((a1 (compile-statement (cadr s) example-env)))
                     ;; evaluate argument, cdr it, then pass to k.
-                    (lambda (env)
+                    (lambda (env k)
                       (a1 env (lambda (v1) (k (cdr v1)))))))
                  ((cons) (check-arity 2)
                   (let ((a1 (compile-statement (cadr s) example-env))
-                        (a2 (compile-statement (nth s 2) example-env)))
+                        (a2 (compile-statement (caddr s) example-env)))
                     ;; evaluate both args, cons them, then pass to k.
                     (lambda (env k)
                       (a1 env (lambda (v1) (a2 env (lambda (v2) (k (cons v1 v2)))))))))
                  ((if) (check-arity 3)
                   (let ((a1 (compile-statement (cadr s) example-env))
-                        (a2 (compile-statement (nth s 2) example-env))
-                        (a3 (compile-statement (nth s 3) example-env)))
+                        (a2 (compile-statement (caddr s) example-env))
+                        (a3 (compile-statement (cadddr s) example-env)))
                     ;; evaluate predicate, if valid, eval a2 otherwise eval a3
                     ;; with the continuation as k
                     (lambda (env k)
                       (a1 env (lambda (v1) ((if v1 a2 a3) env k))))))
-                 ((lambda) 
-                  (let* ((extend-env (lambda (arg-vals)
-                                       (cons (map (partial apply cons) (zip (cadr s) arg-vals)) example-env)))
-                         (extended-env (extend-env (map (constantly '()) (cadr s))))
-                         (compiled-statements (map (lambda (s) (compile-statement s extended-env)) (cddr s))))
-                    (letrec ((eval-statements (lambda (statements)
-                                                (let ((statement (car statements)))
-                                                (if (null? (cdr statements))
+                 ((lambda)
+                  (let* (		; this function builds an
+					; application environment
+					; using the provided args
+			 (extend-env (lambda (env arg-vals)
+                                       (cons (map-args (cadr s) arg-vals) env)))
+			 ;; this is the compile-time environment, with
+			 ;; all variables unbound. This is primarily
+			 ;; used to build the accessor functions at compile-time.
+                         (extended-env (extend-env example-env (make-list (length (cadr s)) '())))
+			 ;; compile all the statements in the body of
+			 ;; the lambda.
+                         (compiled-statements (map (lambda (s) (compile-statement s extended-env)) (cddr s)))
+			 ;; link them together into a single
+			 ;; executable function that returns the value
+			 ;; of the last s-exp.
+			 (evaled-statements (let loop ((statements compiled-statements))
+						(if (null? (cdr statements))
                                                     ;; if this is the last statement evaluate it with continuation
                                                     ;; as k.
-                                                    (lambda (env k) (statement env k))
-                                                    (let ((r (eval-statements (cdr statements))))
+                                                    (lambda (env k) ((car statements) env k))
+                                                    (let ((r (loop (cdr statements))))
                                                       ;; if there's more statements, evaluate this one, discard the
                                                       ;; result and go to the next one.
-                                                      (lambda (env k) (statements env (lambda (_) (r env k))))))))))
-                      (let ((evaled-statements (eval-statements compiled-statements)))
-                        ;; pass lambda to k directly
-                        (lambda (_ k) (k (lambda args
-                                           (unless (= (count (cadr s)) (count args))
-                                             (error "arity mismatch"))
-                                           ;; eval each statement and return the last as result.
-                                           (evaled-statements (extend-env args) identity))))))))
+                                                      (lambda (env k) ((car statements) env (lambda (_) (r env k)))))))))
+                    ;; pass lambda to k directly
+                    (lambda (creation-env k)
+		      (k (lambda args
+			   (unless (= (length (cadr s)) (length args))
+                             (error "arity mismatch"))
+                           ;; eval each statement and return the last
+			   ;; as result, and eval them under the
+			   ;; environment that existed when the lambda
+			   ;; was created, not when it was compiled,
+			   ;; or when it was ran.
+                           (evaled-statements (extend-env creation-env args) identity))))))
                  ((define) (check-arity 2)
-                  (let ((rval (compile-statement (nth s 2) example-env)))
+		  (when (> (length example-env) 1)
+		    (error "use of define within non-global lexical scope"))
+                  (let ((rval (compile-statement (caddr s) example-env)))
                     ;; eval argument, store it in env, then call k with null
                     (lambda (env k)
                       (rval env (lambda (r)
-                                  (if (null? (car env))
-                                      (set-car! env (cons (cons (cadr s) r) '()))
-                                      (set-cdr! (last-cons (car env)) (cons (cons (cadr s) r) '())))
+				  (let ((binding (assoc (cadr s) (last env))))
+				    (if binding
+					(if (null? (cdr binding))
+					    (set-cdr! binding r)
+					    (error (string-append (symbol->string (cadr s))
+								  " is already defined!")))
+					(begin (add-global-binding! env (cadr s))
+					       (set-cdr! (assoc (cadr s) (car env)) r))))
                                   (k '()))))))
                  ((set!) (check-arity 2)
                   (let ((find-pair (get-assoc example-env (cadr s)))
-                        (rval (compile-statement (nth s 2) example-env)))
+                        (rval (compile-statement (caddr s) example-env)))
                     ;; eval argument, store it in the appropriate cell in env, then call k with null
                     (lambda (env k)
                       (rval env (lambda (r)
