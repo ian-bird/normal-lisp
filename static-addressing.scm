4bdfca7d62d4155f7cc9f76cce3393da8fceb177
ian-bird <41979749+ian-bird@users.noreply.github.com>
2026-03-31 02:14:45 -0500

    add more docs + macros
    add a full compile-time macro system

--- static-addressing.scm
+++ static-addressing.scm
@@ -8,27 +8,18 @@
       #t
       #f))
 
-(define (constantly c) (lambda (_) c))
-
-(define (partial fn . args) (lambda rest-args (apply fn (append args rest-args))))
-
-(define (compose first-fn . rest-fns)
-  (if (null? rest-fns)
-      first-fn
-      (let ((rest-fn (apply compose rest-fns)))
-	(lambda args
-	  (first-fn (apply rest-fn args))))))
-
-(define (from-right fn) (compose (lambda (x) (if (list? x) (reverse x) x)) fn reverse))
-
-(define last (from-right car))
-
-(define butlast (from-right cdr))
-
 ;; creates a lookup function to get the variable mentioned.
-;; the lookup uses no counters and should be as fast as a car/cdr chain.
-;; if the variable is not found, it returns a function that performs
-;; a dynamic lookup for the variable.
+;;
+;; this returns a lambda that locates the variable in constant time,
+;; by returning either a closure of the compile-time generated global
+;; binding or a vector ref to the future location of the value in the
+;; lexical environment.
+;;
+;; in this way, the binding strategy can be described as *deep
+;; binding*, which means our memory usage and time complexity at
+;; creation time is linear wrt to the number of lexical frames deep we
+;; are. Shallow binding would make memory usage and creation time
+;; complexity linear wrt the number of referenced variables closed.
 (define (get-assoc env var)
   (let ((lexical-envs (car env))
 	(get-vec-num (lambda (vec k)
@@ -36,6 +27,12 @@
 			 (cond ((>= n (vector-length vec)) #f)
 			       ((eq? (car (vector-ref vec n)) k) n )
 			       (else (loop (1+ n))))))))
+    ;; it's compile time right now. If we can find the binding in the
+    ;; lexical scope, we know exactly where it will appear at
+    ;; runtime. so we can close those numbers for the frame and
+    ;; binding, and return a fn that takes the runtime environment and
+    ;; in 2 vector refs returns the correct binding. This guarantees
+    ;; O(1) lookup for lexical variables.
     (let ((lexical-accessor (let loop ((frame-num 0))
 			      (if (>= frame-num (vector-length lexical-envs))
 				  #f
@@ -49,6 +46,12 @@
 					(loop (1+ frame-num))))))))
       (if lexical-accessor
 	  lexical-accessor
+	  ;; if we didn't find it there, it must be in the global
+	  ;; environment. So we'll add an uninitialized slot for it if
+	  ;; it isn't there, and (still at compile time) returning the
+	  ;; binding as it appears in the global scope. At runtime,
+	  ;; anything accessing this effectively has a pointer to the
+	  ;; exact value they want.
 	  (let ((binding (assoc var (cadr env))))
 	    (unless binding
               (add-global-binding! env var)
@@ -57,21 +60,34 @@
               binding))))))
 
 (define (run-code . statements)
-  (let ((env (list #() '()))
+  (let ((env (list #() '() '()))
         (result '()))
     (for-each (lambda (statement)
 		(set! result ((comp statement env) env identity)))
 	      statements)
     result))
 
+
+
 ;; add an unbound definition to the global environment
 ;; since define can only run in the global level,
 ;; anything not in the lexical scope at compile time must be present here.
+;; 
+;; the exception that is raised should always be caught by a
+;; non-unwinding exception handle. It's to be treated as a warning,
+;; that code higher up in the dynamic stack has the ability to
+;; intercept and modify on its way up to the final logger.
 (define (add-global-binding! env var)
   (raise-exception var #:continuable? #t)
   (if (null? (cadr env))
       (set-car! (cdr env) (list (list var)))
       (set-cdr! (last-pair (cadr env)) (list (list var)))))
+
+(define (add-macro-binding! env var)
+  (raise-exception var #:continuable? #t)
+  (if (null? (caddr env))
+      (set-car! (cddr env) (list (list var)))
+      (set-cdr! (last-pair (caddr env)) (list (list var)))))
 
 (define (map-args params args)
   (let ((v (make-vector (let loop ((params params)
@@ -95,6 +111,15 @@
     (vector-copy! result 1 vec)
     result))
 
+;;  this wraps compile-statement in the appropriate error handling
+;;  code.Warnings are caught, its determined whether the host has the
+;;  capability to handle the unbound, the appropriate warning is
+;;  emited and control passes back to the point where the warning was
+;;  raised.
+;;
+;; This also creates the point for non-local exits to be taken after a
+;; reified continuation returns a final value, and propogates all
+;; other exceptions further up.
 (define (comp s env)
   (lambda (runtime-env k)
     (with-exception-handler
@@ -117,22 +142,41 @@
 	    ((compile-statement s env) runtime-env k))))
       #:unwind? #t)))
 
-;; analyzes the code, combining lambdas into a fn that produces the desired behavior.
-;; compiling thus splits syntactical analysis / symbol location from executing the code.
+;; analyzes the code, combining lambdas into a fn that produces the
+;; desired behavior.  compiling thus splits syntactical analysis /
+;; symbol location from executing the code.
+;;
+;;
+;; note: define is only allowed at the global level. This a critical
+;; restriction of the interpreter, and means that all the variables in
+;; a lexical scope that exist at compile time are exactly the ones
+;; that will exist at runtime. Additional bindings in the lexical
+;; scope cannot be added at runtime precisely because of this
+;; restriction.
+;;
+;; That means any missing binding at runtime can *only* exist in the
+;; global scope, which allows us to, at compile time, replace that
+;; missing symbol with a constant-time accessor to (at this point
+;; allocated ) unbound cell that will hold the value, if the user
+;; provides it.
 (define (compile-statement s example-env)
   (let ((check-arity (lambda (n)
                        (unless (= (length s) (1+ n)) 
-                         (error (string-append
-                                 "arity mismatch: expected " 
-                                 (number->string n)
-                                 " found "
-                                 (number->string (1- (length s)))))))))
+                         (error (format #f "arity mismatch: expected ~a, found ~a"
+					(number->string n)
+					(number->string (1- (length s)))))))))
     (cond ((symbol? s)
            (let ((find-pair (get-assoc example-env s)))
              ;; if found then pass to k, otherwise eval and pass to k.
              (lambda (env k) 
                (let ((p (find-pair env)))
-                 (k (if (and p (not (null? (cdr p)))) (cadr p) (eval s (current-module))))))))
+                 (k (if  (null? (cdr p))
+			 (begin
+                           (when (assoc s (caddr env))
+			     (error (format #f "cannot use macro '~a' as variable"
+					    (symbol->string s))))
+			   (eval s (current-module)))
+			 (cadr p)))))))
           ((any? (lambda (proc) (proc s)) (list boolean? string? number?) )
            ;; self-evaluating. pass value to k directly.
            (lambda (_env k) (k s)))
@@ -215,8 +259,20 @@
 			   ;; or when it was ran.
                            (evaled-statements (extend-env creation-env args) identity))))))
                  ((define) (check-arity 2)
+		  ;; thus, define must also differentiate between
+		  ;; bound variables (whose redefinition is
+		  ;; forbidden), and unbound variables, who exist in
+		  ;; the global namespace but *can* be defined,
+		  ;; because semantically, inside the language, there
+		  ;; cannot be any observable difference between an
+		  ;; existant but unbound variable and a non-existant one.
+		  ;;
 		  (when (positive? (vector-length (car example-env)))
 		    (error "use of define within non-global scope"))
+		  ;; we can detect an invalid use of a macro at compile time
+		  (when (assoc (cadr s) (caddr example-env))
+		    (error (format #f "cannot use macro '~a' as variable"
+				   (symbol->string (cadr s)))))
                   (let* (		; catch self def looks for
 					; exceptions raised for
 					; potentially undefined
@@ -233,8 +289,8 @@
 					     maybe-throws)))
 			 (rval (catch-self-def (lambda ()
 						 (compile-statement (caddr s) example-env)))))
-                    ;; eval argument, store it in env, then call k with null
-                    (lambda (env k)
+		    ;; eval argument, store it in env, then call k with null
+		    (lambda (env k)
                       (rval env (lambda (r)
 				  (let ((binding (assoc (cadr s) (cadr env))))
 				    ;;  if the binding exists and has
@@ -258,7 +314,47 @@
 					   (add-global-binding! env (cadr s))
 					   (set-cdr! (assoc (cadr s) (cadr env)) (list r)))))
 				    (k '())))))))
+		 ((define-macro)
+		  (when (positive? (vector-length (car example-env)))
+		    (error "use of define within non-global scope"))
+                  (let* (		; catch self def looks for
+					; exceptions raised for
+					; potentially undefined
+					; symbols referencing the one
+					; were about to create. Any of
+					; those we encounter we can supress.
+			 (catch-self-def (lambda (maybe-throws)
+					   (with-exception-handler
+					       (lambda (exn)
+						 (unless (symbol? exn)
+						   (raise-exception exn))
+						 (unless (eq? exn (cadr s))
+						   (raise-exception exn #:continuable? #t)))
+					     maybe-throws)))
+			 (rval (catch-self-def (lambda ()
+						 (compile-statement (caddr s) example-env)))))
+		    ;; eval argument, store it in env, then call k with null
+		    (lambda (env k)
+                      (rval env (lambda (r)
+				  (let ((binding (assoc (cadr s) (caddr env))))
+                                    ;; unlike variables, macros can be
+                                    ;; redefined, since set! does not work for them.
+				    (if binding
+                                        (set-cdr! binding (list r))
+					(catch-self-def
+					 (lambda ()
+					   ;;  add the macro binding
+					   ;;  to the current env
+					   ;;  using the current name
+					   ;;  and the evaluation
+					   ;;  result of the 2nd arg
+					   (add-macro-binding! env (cadr s))
+                                           (set-cdr! (assoc (cadr s) (caddr env)) (list r)))))
+				    (k '())))))))
                  ((set!) (check-arity 2)
+		  (when (assoc (cadr s) (caddr example-env))
+		    (error (format #f "cannot use macro '~a' as variable"
+				   (symbol->string (cadr s)))))
                   (let ((find-pair (get-assoc example-env (cadr s)))
                         (rval (compile-statement (caddr s) example-env)))
                     ;; eval argument, store it in the appropriate cell
@@ -299,22 +395,40 @@
 			       (f (lambda (v)
 				    (raise-exception (list 'jump (k v)))))))))))
                  (else			;fn application
-                  (letrec ((fn (compile-statement (car s) example-env))
-                           (args (map (lambda (s) (compile-statement s example-env))
-                                      (cdr s)))
-                           (eval-args (lambda (args)
-                                        (if (null? args)
-                                            (lambda (env k) (k '()))
-                                            (let ((f (car args))
-                                                  (r (eval-args (cdr args))))
-                                              (lambda (env k) (f env (lambda (f)
-                                                                       (r env (lambda (r)
-                                                                                (k (cons f r))))))))))))
-                    (let ((evaled-args (eval-args args)))
-                      ;; eval the function, then eval the args, then apply the fn and pass the result to k
-                      (lambda (env k)
-                        (fn env (lambda (fn)
-                                  (evaled-args env (lambda (args)
-                                                     (k (apply fn args)))))))))))))))
-
-
+		  (cond ((symbol? (car s))
+			 (if 		; if we find the binding
+					; already defined in the
+					; global environment at
+					; compile-time or cannot find
+					; it in the macro environment,
+					;
+					; then compile it as a
+					; function like normal.
+					;
+					; otherwise expand the macro
+					; and resume compilation.
+			  (or (assoc (car s) (cadr example-env))
+				 (not (assoc (car s) (caddr example-env))))
+			  (letrec ((fn (compile-statement (car s) example-env))
+				      (args (map (lambda (s) (compile-statement s example-env))
+						 (cdr s)))
+				      (eval-args (lambda (args)
+						   (if (null? args)
+						       (lambda (env k) (k '()))
+						       (let ((f (car args))
+							     (r (eval-args (cdr args))))
+							 (lambda (env k) (f env (lambda (f)
+										  (r env (lambda (r)
+											   (k (cons f r))))))))))))
+			       (let ((evaled-args (eval-args args)))
+				 ;; eval the function, then eval the
+				 ;; args, then apply the fn and pass
+				 ;; the result to k
+				 (lambda (env k)
+				   (fn env (lambda (fn)
+					     (evaled-args env (lambda (args)
+								(k (apply fn args)))))))))
+			  (let* ((macro (cadr (assoc (car s) (caddr example-env))))
+				 (args (cdr s))
+				 (expansion (apply macro args)))
+			    (compile-statement expansion example-env)))))))))))
